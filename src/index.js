c77dcc1cc760eefec19362b1fe6d0f42fe2c1c3e
TomJS14 <133436558+TomJS14@users.noreply.github.com>
2024-01-20 21:30:40 +0000

    Added second page & scroll behaviour

--- src/index.js
+++ src/index.js
@@ -1,47 +1,97 @@
 /** @format */
 
 import "./styles.css";
-import background from "./gradientBG.jpg";
 import wavingHand from "./openHand.svg";
+import webDesign from "./website-design.png";
 const main = document.querySelector(".main");
 
-function RenderComponent() {
-  const element = document.createElement("div");
-  element.textContent = "Hi! I'm Tom";
-  element.classList.add("hello");
-  //Background element
-  const myBackground = new Image();
-  myBackground.src = background;
-  myBackground.classList.add("backgroundImg");
-  element.appendChild(myBackground);
+function RenderPage() {
+  //create container
+  const container = document.createElement("div");
+  container.classList.add("container");
+
+  //Slide 1 elements
+  const slideOne = document.createElement("div");
+  const slideOneHeader = document.createElement("h2");
+  slideOneHeader.textContent = "Hi! I'm Tom";
+  slideOneHeader.classList.add("hello");
+  const messageContainer = document.createElement("div");
+  slideOne.classList.add("slides");
+  slideOne.classList.add("slideOne");
+
   //Hand icon
   const myHand = document.createElement("object");
   myHand.data = wavingHand;
   myHand.type = "image/svg+xml";
   myHand.classList.add("waving");
-  element.appendChild(myHand);
 
-  //Call to content
+  //message box
+  const messageBox = document.createElement("div");
+  messageBox.textContent = "Placeholder";
+  messageBox.classList.add("projectContainer");
 
-  return element;
+  //append elements
+  slideOne.appendChild(slideOneHeader);
+  slideOne.appendChild(myHand);
+  slideOne.appendChild(messageBox);
+  container.appendChild(slideOne);
+
+  //Night mode button
+  const nightSwitch = document.createElement("button");
+  nightSwitch.classList.add("LightMode");
+  nightSwitch.classList.add("nightSwitch");
+  nightSwitch.textContent = "Light mode";
+  slideOne.appendChild(nightSwitch);
+
+  //content message
+  const message = document.createElement("h1");
+  message.textContent = "Check out my projects";
+  message.classList.add("message");
+  slideOne.appendChild(message);
+
+  message.addEventListener("mouseenter", handleHover);
+  message.addEventListener("mouseleave", handleHover);
+
+  //slide 2 & elements
+  const slideTwo = document.createElement("div");
+  const slideTwoHeader = document.createElement("h2");
+  /* const slideTwoImage = document.createElement("img"); */
+  const slideTwoImage = new Image();
+  slideTwoImage.src = webDesign;
+
+  slideTwoHeader.textContent = "I'm a developer";
+  slideTwoHeader.classList.add("hello");
+  slideTwo.classList.add("slides");
+  slideTwo.classList.add("slideTwo");
+
+  slideTwo.appendChild(slideTwoHeader);
+  slideTwo.appendChild(slideTwoImage);
+  container.appendChild(slideTwo);
+
+  //append everything to main
+  main.appendChild(container);
+
+  return container;
 }
 
-main.appendChild(RenderComponent());
+const pageContainer = RenderPage();
 
-//Night mode button
+function handleHover() {
+  // Calculate the scroll position based on the message's hover state
+  const scrollPosition = this.classList.contains("hovered")
+    ? 0
+    : this.clientHeight + 300; //Adjust the height value
+  //set to scroll to the above position smoothly
+  window.scrollTo({
+    top: scrollPosition,
+    behavior: "smooth",
+  });
 
-const nightSwitch = document.createElement("button");
-nightSwitch.classList.add("LightMode");
-nightSwitch.classList.add("nightSwitch");
-nightSwitch.textContent = "Light mode";
-main.appendChild(nightSwitch);
+  //toggle the hovered class to reverse the scroll??
+  /* this.classList.toggle("hovered"); */
+}
 
-//content message
-const message = document.createElement("div");
-message.textContent = "Check out my projects";
-message.classList.add("message");
-main.appendChild(message);
-
+const nightSwitch = pageContainer.querySelector(".nightSwitch");
 nightSwitch.addEventListener("click", DarkModeToggle);
 
 function DarkModeToggle(myHand) {
@@ -62,3 +112,5 @@ function DarkModeToggle(myHand) {
     pathElement.style.fill = "white"; //update the fill of the svg
   }
 }
+
+//projects container
