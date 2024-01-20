beb02f2309707e2ceca602062c5f565e635d705b
TomJS14 <133436558+TomJS14@users.noreply.github.com>
2024-01-20 16:54:02 +0000

    Updated with welcome message

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import "./styles.css";
 import background from "./gradientBG.jpg";
-import wavingHand from "./waveHandVector.png";
+import wavingHand from "./openHand.svg";
 const main = document.querySelector(".main");
 
 function RenderComponent() {
@@ -15,11 +15,50 @@ function RenderComponent() {
   myBackground.classList.add("backgroundImg");
   element.appendChild(myBackground);
   //Hand icon
-  const myHand = new Image();
-  myHand.src = wavingHand;
+  const myHand = document.createElement("object");
+  myHand.data = wavingHand;
+  myHand.type = "image/svg+xml";
   myHand.classList.add("waving");
   element.appendChild(myHand);
+
+  //Call to content
+
   return element;
 }
 
 main.appendChild(RenderComponent());
+
+//Night mode button
+
+const nightSwitch = document.createElement("button");
+nightSwitch.classList.add("LightMode");
+nightSwitch.classList.add("nightSwitch");
+nightSwitch.textContent = "Light mode";
+main.appendChild(nightSwitch);
+
+//content message
+const message = document.createElement("div");
+message.textContent = "Check out my projects";
+message.classList.add("message");
+main.appendChild(message);
+
+nightSwitch.addEventListener("click", DarkModeToggle);
+
+function DarkModeToggle(myHand) {
+  const objectElement = document.querySelector(".waving");
+  const svgDoc = objectElement.contentDocument; //access the svg document
+  const pathElement = svgDoc.querySelector("path");
+  if (nightSwitch.classList.contains("darkMode")) {
+    nightSwitch.classList.remove("darkMode");
+    nightSwitch.classList.add("lightMode");
+    nightSwitch.textContent = "Light Mode";
+    main.classList.remove("dark");
+    pathElement.style.fill = "black"; //update the fill of the svg
+  } else {
+    nightSwitch.classList.remove("lightMode");
+    nightSwitch.classList.add("darkMode");
+    nightSwitch.textContent = "Dark Mode";
+    main.classList.add("dark");
+    pathElement.style.fill = "white"; //update the fill of the svg
+  }
+}
